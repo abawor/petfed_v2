86db39e4429eccbaa8a6af404b3eb28218123744
tensARC-ABawor <abawor200@gmail.com>
2025-03-12 09:54:06 +0000

    Added test for App for rendering links to 4 main sections

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it } from "vitest";
-import { render } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import petsReducer from "../redux/Pets.tsx";
@@ -18,5 +18,22 @@ describe("PetList Component", () => {
                 <App />
              </Provider>
         );
+    });
+
+    it("Renders Route links", () => {
+        render(
+            <Provider store={loadingStore}>
+                <App />
+             </Provider>
+        );
+
+        const petsLink = screen.getByText("Pets");
+        const mealsLink = screen.getByText("Meals");
+        const scheduleLink = screen.getByText("Schedule");
+        const settingsLink = screen.getByText("Settings");
+        expect(petsLink).toBeInTheDocument();
+        expect(mealsLink).toBeInTheDocument();
+        expect(scheduleLink).toBeInTheDocument();
+        expect(settingsLink).toBeInTheDocument();
     });
 });
